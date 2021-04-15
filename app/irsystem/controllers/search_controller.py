8ddb98f49ea33bfdd83dd3e2bd5335a5ed85e2d4
Tricia Park <tp294@cornell.edu>
2021-04-15 11:40:15 -0400

    small change to make_vector

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,6 +11,8 @@ def search():
 	# make_vector(request.args)
 	# render_results(results)
 
+	print(request.args)
+
 	query = request.args.get('apartment')
 	if not query:
 		data = []
@@ -23,13 +25,23 @@ def search():
 
 def make_vector(traits):
 	"""
-	Input: traits (dictionary of integer traits that user inputs)
+	Input: traits (dictionary of integer traits that user inputs, different based on dog or cat)
 	Example: [('apartment', '3'), ('novice', '3'), ('sensitivity', '3'), ('alone', '3'), ('cold', '3'), ('hot', '3'), ('family-dog', '3'), ('kids-dog', '3'), ('dog-friendly', '3'), ('strangers', '3'), ('shedding-dog', '3'), ('drool', '3'), ('groom-dog', '3'), ('health-dog', '3'), ('weight-gain', '3'), ('size', '3'), ('train', '3'), ('intelligence-dog', '3'), ('mouthiness', '3'), ('prey', '3'), ('noise', '3'), ('wander', '3'), ('energy', '3'), ('intensity', '3'), ('exercise', '3'), ('playful', '3')]
 	
-	Output: vector (list of ints)
+	Output: vector (list of ints, different based on dog or cat)
 	Example: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+
+	The "dog-selected" or "cat-selected" field will be in the input traits 
+	depending on if the dog form or if the cat form was selected
 	"""
-	raise NotImplementedError()
+	output = []
+	if request.args.get('dog-selected') is not None:
+		# process input for dog form
+		continue
+	elif request.args.get('cat-selected') is not None:
+		# process input for cat form
+		continue
+	return output
 
 def render_results(results):
 	"""
