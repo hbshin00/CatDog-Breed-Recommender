5d7af31af7af85f243d0a081a68f040aff38af5d
Tricia Park <tp294@cornell.edu>
2021-04-17 13:01:12 -0400

    added some comments

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -59,6 +59,7 @@ def make_vector(traits):
 	
 	Output: vector (list of ints, different based on dog or cat)
 	Example: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+	output must be in order that traits appear in dataset/website
 
 	The "dog-selected" or "cat-selected" field will be in the input traits 
 	depending on if the dog form or if the cat form was selected
@@ -66,10 +67,10 @@ def make_vector(traits):
 	output = []
 	if request.args.get('dog-selected') is not None:
 		# process input for dog form
-		output = [int(x[1]) for x in traits]
+		output = [int(x[1]) for x in traits] #this may include some extra input from the dog-selected/cat-selected fields, may be better to get each input manually with .get(input)
 	elif request.args.get('cat-selected') is not None:
 		# process input for cat form
-		output = [int(x[1]) for x in traits]
+		output = [int(x[1]) for x in traits] #same as above, reference "traits" input for names of fields
 	return output
 
 def render_results(results):
