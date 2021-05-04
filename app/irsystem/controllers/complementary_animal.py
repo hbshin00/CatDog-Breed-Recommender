775874f3551560bc1b70672afc2d2f91da4b503e
matteogsavarese <matteo@matteosavarese.com>
2021-05-04 18:04:44 -0400

    novelty matrix update

--- app/irsystem/controllers/complementary_animal.py
+++ app/irsystem/controllers/complementary_animal.py
@@ -1,7 +1,8 @@
-# from . import * efqef
 import numpy as np
 import pandas as pd
 import pickle
+import math
+import csv
 
 df1 = pd.read_csv("data/dogs.csv")
 dog_data = df1.values.tolist()
@@ -17,6 +18,8 @@ for cat in cat_data:
     vector_list = cat[7:]
     vector = np.asarray(vector_list)
     cat_vectors.append(vector)
+cat_vectors[56] = np.asarray([1,0,0,0,0,0,0,0])
+dog_vectors[150][25] = 4
 
 altered_dog_vectors = []
 # Affectionate with Family [6],Amount of Shedding [10],General Health [13],
@@ -34,17 +37,50 @@ for vector in dog_vectors:
     new_vector = np.asarray(new_vector_list)
     altered_dog_vectors.append(new_vector)
 
+# max diffrence 28
 num_dogs = len(altered_dog_vectors)
 num_cats = len(cat_vectors)
 good_companion_matrix = np.zeros((num_dogs,num_cats))
 for i in range(num_dogs):
     dv = altered_dog_vectors[i]
+    dv1 = dv[:7]
+    dv2 = dv[7]
     for j in range(num_cats):
         cv = cat_vectors[j]
-        a = cv-dv
+        cv1 = cv[:7]
+        cv2 = cv[7]
+        a = cv1-dv1
         b = np.absolute(a)
         c = np.sum(b)
-        sim = (32-c)/32
+        sim1 = (22-c)/22
+        # pet_social_score is the combined value of both animals Pet Friendly value
+        pet_social_score = dv2 + cv2
+        if pet_social_score == 2:
+            sim2 = -0.5
+        elif pet_social_score == 3:
+            sim2 = -0.3
+        elif pet_social_score == 4:
+            sim2 = -0.1
+        elif pet_social_score == 5:
+            sim2 = 0
+        elif pet_social_score == 6:
+            sim2 = 0.25
+        elif pet_social_score == 7:
+            sim2 = 0.5
+        elif pet_social_score == 8:
+            sim2 = 0.75
+        elif pet_social_score == 9:
+            sim2 = 0.9
+        elif pet_social_score == 10:
+            sim2 = 1.1
+        else:
+            sim2 = 0
+        sim3 = (sim1*0.65) + (sim2 * 0.35)
+        if sim3 < 0.05:
+            sim4 = 0.05
+        else:
+            sim4 = sim3
+        sim = round(sim4,2)
         good_companion_matrix[i,j] = sim
 
 df = pd.DataFrame(good_companion_matrix)
@@ -52,8 +88,6 @@ df.to_csv('data/matrix.csv')
 
 # with open("good_companion_matrix.pickle","wb") as fp:
 #     pickle.dump(good_companion_matrix, fp)
-
-# pickle.load rb
 
 
 # Affectionate with Family [6],Amount of Shedding [10],General Health [13],
